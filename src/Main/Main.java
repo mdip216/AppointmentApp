74fcc4f096c4333cd0f582b24b7f9b1c31bac1ad
Matt DiPerna <mdiperna216@gmail.com>
2022-01-22 14:24:40 -0500

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,27 @@
+package Main;
+
+import Util.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../View/Login.fxml"));
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        JDBC.openConnection();
+        JDBC.closeConnection();
+
+
+    }
+}
